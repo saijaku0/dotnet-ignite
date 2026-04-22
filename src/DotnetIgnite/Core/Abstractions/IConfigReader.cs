ddc1b16ecd5708d1526fc7f4dfa98eb9456c2b9b
Vadym <minerking112256@gmail.com>
2026-04-22 20:48:59 +0200

    docs: add docs

--- src/DotnetIgnite/Core/Abstractions/IConfigReader.cs
+++ src/DotnetIgnite/Core/Abstractions/IConfigReader.cs
@@ -4,5 +4,9 @@ namespace DotnetIgnite.Core.Abstractions;
 
 public interface IConfigReader
 {
+    /// <summary>
+    /// Loads the Ignite configuration from the current working directory.
+    /// </summary>
+    /// <returns>An instance of IgniteConfig representing the loaded configuration.</returns>
     IgniteConfig LoadFromCurrentDirectory();
 }
--- src/DotnetIgnite/Core/Abstractions/IFileSystem.cs
+++ src/DotnetIgnite/Core/Abstractions/IFileSystem.cs
@@ -2,8 +2,38 @@ namespace DotnetIgnite.Core.Abstractions;
 
 public interface IFileSystem
 {
+    /// <summary>
+    /// Determines whether the specified file exists.
+    /// </summary>
+    /// <param name="path">The path to the file.</param>
+    /// <returns>true if the file exists; otherwise, false.</returns>
     bool FileExists(string path);
+    /// <summary>
+    /// Opens a file, reads all text, and then closes the file.
+    /// </summary>
+    /// <param name="path">The file to open for reading. Can be a relative or absolute path.</param>
+    /// <returns>A string containing all text in the file.</returns>
     string ReadAllText(string path);
+    /// <summary>
+    /// Gets the current working directory of the application.
+    /// </summary>
+    /// <returns>The path of the current working directory.</returns>
     string GetCurrentDirectory();
+    /// <summary>
+    /// Combines two strings into a path.
+    /// </summary>
+    /// <param name="pathA">The first path to combine.</param>
+    /// <param name="pathB">The second path to combine.</param>
+    /// <returns>
+    /// The combined paths. If one of the specified paths is a zero-length string,
+    /// this method returns the other path. If <paramref name="pathB"/> contains an absolute path,
+    /// this method returns <paramref name="pathB"/>.
+    /// </returns>
+    /// <exception cref="System.ArgumentNullException">
+    /// <paramref name="pathA"/> or <paramref name="pathB"/> is null.
+    /// </exception>
+    /// <exception cref="System.ArgumentException">
+    /// <paramref name="pathA"/> or <paramref name="pathB"/> contains invalid characters.
+    /// </exception>
     string CombinePath(string pathA, string pathB);
 }
